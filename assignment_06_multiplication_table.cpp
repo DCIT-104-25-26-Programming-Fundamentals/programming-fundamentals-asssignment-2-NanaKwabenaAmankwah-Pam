7c1727b4fb2bd4c9a75ce3e5877fcff7c5fae54e
NanaKwabenaAmankwah-Pam <kamankwah010@st.ug.edu.gh>
2026-07-31 22:14:01 +0000

    assignment_06

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,63 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+void printSingleTable(int number)
+{
+    cout << "\nMultiplication Table for " << number << ":\n";
+
+    for (int i = 1; i <= 12; i++)
+    {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+}
+
+void printTables(int n)
+{
+    if (n <= 0)
+    {
+        cout << "Error: Number must be greater than 0." << endl;
+        return;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        printSingleTable(i);
+
+        if (i != n)
+        {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main()
+{
+    int number, n;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number <= 0)
+    {
+        cout << "Error: Number must be greater than 0." << endl;
+        return 0;
+    }
+
+    printSingleTable(number);
+
+    cout << "\nEnter a number N: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: Number must be greater than 0." << endl;
+        return 0;
+    }
+
+    printTables(n);
+
+    return 0;
+}
